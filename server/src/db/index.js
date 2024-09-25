94d4fb2f7f872d8b2c33db4adc4402e9808a720e
miigo-dev <miigo.mnl@gmail.com>
2024-09-25 23:23:45 +0800

    require dotenv for db connection

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -9,7 +10,6 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 });
 
-pool.on('error', (err, client) => {
-    console.error('Unexpected error on idle client', err);
-    process.exit(-1);
-});
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+};
