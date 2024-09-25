2903919f4517cae0273a470ee2c354261ffcc65c
Krista Noelle Cruz <kristanoellecruz@gmail.com>
2024-09-25 18:03:13 +0800

    Added login.js, created css folder for design , and login.css

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,9 +1,42 @@
+import React from 'react'
+import '../css/login.css';
+
+
 const login = () => {
-  return (
-    <div>
-        <h1>Login</h1>
-    </div>
-  )
+
+    return (
+        <div className='login_container'>
+            <div className='login_box'>
+                <div className='logo_container'>
+                    <img src='lakbaylogo.png' alt='Lakbay Logo' className='logo'/>
+                </div>
+
+            <h2>Lakbay Kape and Kain</h2>
+            <p>Login to your account</p>
+
+                <form>
+                    <div className='form_input'>
+                        <label htmlFor='email' className='email_icon'>
+                        <img src="envelope_icon.png" alt="email icon" className="input_icon"/>
+                        </label>  
+                        <input type='email' id='email' placeholder='Email Address' required />
+                    </div>
+
+                    <div className='form_input'>
+                        <label htmlFor='password' className='password_icon'>
+                        <img src="password_icon.png" alt="password icon" className="input_icon"/>
+                        </label>  
+                        <input type='password' id='password' placeholder='Password' required />
+                    </div>
+
+                    <div className='forgot_pass'>
+                        <a href='/forgot_password'>Forgot Password</a>
+                    </div>
+                    <button type="submit" className="login_button">Login</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
-export default login
+export default login;
