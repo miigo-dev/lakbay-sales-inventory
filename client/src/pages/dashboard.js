a405458fadb66bedad635980307a903f7f9d5193
miigo-dev <miigo.mnl@gmail.com>
2024-09-25 22:44:11 +0800

    fix

--- client/src/pages/dashboard.js
+++ client/src/pages/dashboard.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { fetchProtectedInfo, onLogout } from '../api/auth'
 import { unauthenticateUser } from '../redux/slices/authSlice'
+import '../css/dashboard.css'
 
 const Dashboard = () => {
   const dispatch = useDispatch()
