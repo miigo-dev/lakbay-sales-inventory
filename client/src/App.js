125b90e3b3d97906c9e03f55f3d16c031ec8588f
miigo-dev <miigo.mnl@gmail.com>
2024-09-27 04:40:57 +0800

    default route to login

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ import {
   Route,
   Outlet
 } from 'react-router-dom'
-import Home from './pages/home'
 import Dashboard from './pages/dashboard'
 import Login from './pages/login'
 import Register from'./pages/register'
