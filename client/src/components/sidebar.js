51f880039ae5dfd2b950acd1b305bf9634a4dd84
miigo-dev <miigo.mnl@gmail.com>
2024-09-28 01:06:04 +0800

    added image assets

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -2,6 +2,15 @@ import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import '../css/sidebar.css'
 
+import logo from '../assets/lakbaylogo50px.png'
+import dash_icon from '../assets/dash_icon_dark.png'
+import order_icon from '../assets/order_icon_dark.png'
+import inventory_icon from '../assets/folder_icon_dark.png'
+
+import reports_icon from '../assets/report_icon_dark.png'
+import users_icon from '../assets/profile_icon25px.png'
+import settings_icon from '../assets/settings_icon_dark.png'
+
 const Sidebar = () => {
   const { isAuth } = useSelector((state) => state.auth);
 
@@ -10,56 +19,58 @@ const Sidebar = () => {
   }
 
   return (
+    <>
     <div className="sidebar">
 
       <div className="header_sidebar">
-        <img src="path/to/logo.png" alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" />
         <h2>Lakbay Kape</h2>
       </div>
 
       <NavLink to="/dashboard" activeClassName="active">
-      <img src="" alt="Dashboard icon" />
+      <img src={dash_icon} alt="Dashboard icon" />
         Dashboard    
       </NavLink>
 
       <NavLink to="/orders" activeClassName="active">
-      <img src="../assets/order_basketicon24px.png" alt="Orders icon" />
+      <img src={order_icon} alt="Orders icon" />
         Orders
       </NavLink>
 
       <NavLink to="/inventory" activeClassName="active">
-      <img src="" alt="Inventory icon" />
+      <img src={inventory_icon} alt="Inventory icon" />
         Inventory
       </NavLink>
 
       <NavLink to="/sales" activeClassName="active">
-      <img src="" alt="Sales icon" />
+      <img src='{sales_icon}' alt="Sales icon" />
         Sales      
       </NavLink>
 
       <NavLink to="/reports" activeClassName="active">
-      <img src="" alt="Reports icon" />
+      <img src={reports_icon} alt="Reports icon" />
         Reports    
       </NavLink>
 
       <NavLink to="/users" activeClassName="active">
-      <img src="" alt="Users icon" />
+      <img src={users_icon} alt="Users icon" />
         Users   
       </NavLink>
 
       <div className="bot_sidebar">
         <NavLink to="/settings" activeClassName="active">
-        <img src="" alt="Settings icon" />
+        <img src={settings_icon} alt="Settings icon" />
           Settings
         </NavLink>
 
         <NavLink to="/logout" activeClassName="active">
-        <img src="" alt="Logout icon" />
+        <img src='' alt="Logout icon" />
           Logout
         </NavLink>
       </div>
 
     </div>
+    </>
   )
 }
 
