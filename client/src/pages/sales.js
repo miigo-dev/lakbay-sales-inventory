d12ac1079fa65d80a012f5ff189beddbdf051d3f
miigo-dev <miigo.mnl@gmail.com>
2024-10-02 06:07:39 +0800

    Refactor CSS styles for sales and products cards

--- client/src/pages/sales.js
+++ client/src/pages/sales.js
@@ -6,45 +6,46 @@ import products_icon from '../assets/icons/products_data.svg'
 const Sales = () => {
   return (
     <div class='container'>
-        <div class='card-sales'>
-          <div class='card-head'>
-            <div class='card-icon'>
-            <h1><span><img src={sales_icon} alt=''></img></span></h1>
+      <div class='card-sales'>
+        <div class='card-icon'>
+          <h1><span><img src={sales_icon} alt=''></img></span></h1>
+        </div>
+          <div class='sales-info-main'>
+            <div class='info-row>'>
+              <span>Sales</span>
+              <h2>PHP 50,000</h2>
             </div>
-            <h2>Sales</h2>
-          </div>
-          <div class='card-info'>
-            <h1>PHP 50,000</h1>
+          <div class='info-row>'>
+            <span>Volume</span>
+            <h2>450</h2>
           </div>
         </div>
+      </div>
 
-        <div class='card-sales'>
-          <div class='card-head'>
-            <div class='card-icon'>
-            <h1><span><img src={products_icon} alt=''></img></span></h1>
-            </div>
-            <div class='sales-info-main'>
+      <div class='card-products'>
+          <div class='card-icon'>
+          <h1><span><img src={products_icon} alt=''></img></span></h1>
+          </div>
+            <div class='products-info-main'>
               <div class='info-row>'>
                 <span>All Orders</span>
+                <h2>450</h2>
               </div>
               <div class='info-row>'>
                 <span>Pending</span>
+                <h2>5</h2>
               </div>
               <div class='info-row>'>
                 <span>Completed</span>
-              </div>
+                <h2>445</h2>
             </div>
-          </div>
-          
-          <div class='card-info'>
-            <h1>PHP 50,000</h1>
-          </div>
         </div>
+      </div>
 
-        <div class='card-summary'>
+      <div class='card-summary'>
         <div class='bar-head'>
-              <h2>Summary</h2>
-          </div>
+          <h2>Summary</h2>
+        </div>
         <BarChart
           xAxis={[
             {
@@ -56,12 +57,14 @@ const Sales = () => {
           series={[
             {
               data: [2, 5, 3, 11, 4, 5, 9],
+              color: '#C2A790'
             },
           ]}
+          borderRadius={100}
           width={500}
           height={300}
-        />
-        </div>
+          />
+      </div>
     </div>
   )
 }
