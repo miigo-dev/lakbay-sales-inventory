41f4b8e5efb7cd6c0132da373256e84473368a43
Angelo Palabay <gelpalabay05@gmail.com>
2024-10-01 23:02:04 +0800

    sales ui dash first

--- client/src/pages/inventory.js
+++ client/src/pages/inventory.js
@@ -1,7 +1,12 @@
+import '../css/inventory.css'
+
 const Inventory = () => {
   return (
+
+    
     <div>
-        <h1>Inventory</h1>
+
+        
     </div>
   )
 }
--- client/src/pages/sales.js
+++ client/src/pages/sales.js
@@ -1,7 +1,12 @@
+import '../css/sales.css'
+
+
 const Sales = () => {
   return (
     <div>
         <h1>Sales</h1>
+
+
     </div>
   )
 }
