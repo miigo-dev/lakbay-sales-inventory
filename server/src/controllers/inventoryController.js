460976a1afcc4e92940537689b065f089eec4cb3
miigo-dev <miigo.mnl@gmail.com>
2024-10-21 17:32:53 +0800

    Refactor inventoryController.js and inventoryRoute.js: Add CRUD endpoints for products

--- server/src/controllers/inventoryController.js
+++ server/src/controllers/inventoryController.js
@@ -18,3 +18,64 @@ exports.getProductByID = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 }
+
+exports.addProduct = async (req, res) => {
+    try {
+        const { warehouse_id, 
+            product_name,
+            category_id,
+            product_quantity,
+            product_price,
+            reorder_level,
+            product_status,
+            remarks
+        } = req.body;
+        const product = await inventoryService.addProduct(warehouse_id, 
+            product_name, 
+            category_id, 
+            product_quantity, 
+            product_price, 
+            reorder_level, 
+            product_status, 
+            remarks
+        );
+        res.status(200).json(product);
+        } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.updateProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { warehouse_id, 
+            product_name,
+            category_id,
+            product_price,
+            reorder_level,
+            product_status,
+            remarks
+        } = req.body;
+        const product = await inventoryService.updateProduct(id, warehouse_id, 
+            product_name, 
+            category_id, 
+            product_price, 
+            reorder_level, 
+            product_status, 
+            remarks
+        );
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await inventoryService.deleteProduct(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
--- server/src/routes/inventoryRoute.js
+++ server/src/routes/inventoryRoute.js
@@ -5,5 +5,8 @@ const inventoryController = require('../controllers/inventoryController');
 
 router.get('/products', inventoryController.getAllProducts);
 router.get('/products/:id', inventoryController.getProductByID);
+router.post('/products', inventoryController.addProduct);
+router.put('/products/:id', inventoryController.updateProduct);
+router.delete('/products/:id', inventoryController.deleteProduct);
 
 module.exports = router;
